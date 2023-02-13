90a9e76c2c585e470496034eb39573439c32bc34
mjminio <mj@min.io>
2023-02-13 15:29:42 -0500

    add cluster delete

--- ansible/roles/minio/files/core/min-lab-core-cluster-delete.sh
+++ ansible/roles/minio/files/core/min-lab-core-cluster-delete.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+CURRENTTIME=`date +"%Y%m%d%H%M%S"`
+sudo mkdir -p /mnt/.backup/$CURRENTTIME
+sudo cp -R /mnt/data/* /mnt/.backup/$CURRENTTIME
+
 docker rm -f minio1
 sudo rm -rf /mnt/data/minio1
 docker rm -f minio2
