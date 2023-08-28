9bef93434d3f852c583fff60f773c672a072a9f4
Allan Reid <allanrogerreid@gmail.com>
2023-08-28 15:41:58 -0700

    Install and use podman-compose as user

--- ansible/roles/minio/files/docker/distributed/minio-docker-compose-start.sh
+++ ansible/roles/minio/files/docker/distributed/minio-docker-compose-start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose -f /home/{{ user }}/minio/tools/deploy/docker/docker-compose/distributed/docker-compose.yml up -d
+/home/ubuntu/.local/bin/podman-compose -f /home/{{ user }}/minio/tools/deploy/docker/docker-compose/distributed/docker-compose.yml up -d
