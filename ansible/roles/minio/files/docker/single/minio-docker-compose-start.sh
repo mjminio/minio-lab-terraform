2bdb8f636a79731e3ab50ca58ad69d1a83fcde39
Allan Roger Reid <allanrogerreid@gmail.com>
2023-09-06 23:59:18 -0700

    Vm broker (#10)
    Need to use podman since docker requires mounting; not possible on LXC
    Linger at start, then reboot

--- ansible/roles/minio/files/docker/single/minio-docker-compose-start.sh
+++ ansible/roles/minio/files/docker/single/minio-docker-compose-start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose -f /home/{{ user }}/minio/tools/deploy/docker/docker-compose/single/docker-compose.yml up -d
+/home/ubuntu/.local/bin/podman-compose -f /home/{{ user }}/minio/tools/deploy/docker/docker-compose/single/docker-compose.yml up -d
